402e95d07e864f61f065ca5e043d2f4266cf4f09
lisns <z.z.iz.0pnup@gmail.com>
2019-07-29 16:35:07 +0900

    Node用クラスの作成(空)、 XMLのサンプルクラスの作成(ロードのサンプル処理のみ作成)

---
+++ mmCreaterCs/XmlSample.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace mmCreaterCs
+{
+    class XmlSample
+    {
+        public XmlSample()
+        {
+            XDocument xml = XDocument.Load(@"C:\tmp\test.xml"); // fopen/close不要
+            XElement table = xml.Element("価格表");
+            var rows = table.Elements("データ");
+            foreach ( XElement row in rows )
+            {
+                string jp = "";
+                string en = "";
+
+                // 商品名
+                foreach ( var item in row.Elements("商品") )
+                {
+                    // 属性ごとに名称をセット
+                    XAttribute attr = item.Attribute("言語");
+                    switch ( attr.Value )
+                    {
+                    case "日本語":
+                        jp = item.Value;
+                        break;
+                    case "英語":
+                        en = item.Value;
+                        break;
+                    default:
+                        break;
+                    }
+                }
+                // 単価
+                XElement rate = row.Element("単価");
+                string price = rate.Value;
+
+                // 出力
+                Console.WriteLine("[jp/en]" + jp + "/" + en + ", rate: " + price);
+            }
+        }
+
+        public load()
+        {
+
+        }
+    }
+}
