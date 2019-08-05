68de72fb978091f7500567433a4277c7ca81ff12
lisns <z.z.iz.0pnup@gmail.com>
2019-08-05 18:38:11 +0900

    ロガーの追加:コンソール出力するだけの仮ログ関数作成

---
+++ mmCreaterCs/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mmCreaterCs
+{
+    class Logger
+    {
+        public void Log(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
