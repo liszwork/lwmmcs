12d692fc5272b6746df12da32b37443ed27f8819
lisns <z.z.iz.0pnup@gmail.com>
2019-08-05 18:38:40 +0900

    ノート生成に必要な各関数の作成(不足あり)

--- mmCreaterCs/Node.cs
+++ mmCreaterCs/Node.cs
@@ -9,6 +9,7 @@ namespace mmCreaterCs
 {
     class Node
     {
+        private Logger logger = new Logger();
         private ulong uniqueNo = 0xFFFFFFFF;
         public Node()
         {
@@ -21,30 +22,57 @@ namespace mmCreaterCs
         /// <param name="text"></param>
         /// <param name="position"></param>
         /// <returns></returns>
-        public XElement CreateNode(string text, string position="")
+        public XElement CreateNode(string text, string position = "")
         {
+            string uniqueNo = GenerateUniqueNo();
+            XElement elm = new XElement("node");
+            // TODO: ユニーク値を何かしらで管理すべき？
+            Dictionary<string, string> attr = new Dictionary<string, string>()
+            {
+                { "CREATED", string.Format("{0:013d}", int.Parse(uniqueNo)) },
+                { "ID", "ID_" + string.Format("{0:09d}", int.Parse(uniqueNo)) },
+                { "MODIFIED", string.Format("{0:013d}", int.Parse(uniqueNo)) },
+                { "TEXT", text }
+            };
+            if ( 0 < position.Length )
+            {
+                attr.Add("POSITION", position);
+            }
+            AddAttr(attr, elm);
+
             return null;
         }
 
+#if false
         /// <summary>
         /// 属性の作成
         /// </summary>
         /// <param name="key"></param>
         /// <param name="val"></param>
         /// <returns></returns>
-        private XAttribute CreateAttr(string key, string val)
-        {
-            return null;
-        }
+        //private XAttribute CreateAttr(string key, string val)
+        //{
+        //    logger.Log("CreateAttr [" + key + "," + val + "]");
+        //    XAttribute attr = new XAttribute(key, val);
+        //    return attr;
+        //}
+#endif
 
         /// <summary>
         /// Nodeに属性を追加する
         /// </summary>
         /// <param name="attrInfo"></param>
         /// <param name="node"></param>
-        private void AddAttr(Dictionary<string, string> attrInfo, XElement node)
+        /// <returns></returns>
+        private XElement AddAttr(Dictionary<string, string> attrInfo, XElement node)
         {
-
+            XElement editNode = node;
+            foreach ( var item in attrInfo )
+            {
+                //XAttribute attr = CreateAttr(item.Key, item.Value);
+                editNode.SetAttributeValue(item.Key, item.Value);
+            }
+            return editNode;
         }
 
         /// <summary>
@@ -56,9 +84,18 @@ namespace mmCreaterCs
             System.DateTime dt = System.DateTime.Now;
             string datetime = String.Format("{0:yyyyMMddHHmmssfff000", dt);
             string msg = String.Format("{0:yyyy年MM月dd日(ddd) HH時mm分ss秒fff}", dt) + "\r\n"
-                + datetime
-            Console.WriteLine(msg);
+                + datetime;
+            logger.Log(msg);
             return datetime;
+        }
+
+        /// <summary>
+        /// ユニーク番号文字列の生成
+        /// </summary>
+        /// <returns></returns>
+        private string GenerateUniqueNo()
+        {
+            return "";
         }
     }
 }
